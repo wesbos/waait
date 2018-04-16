81c7c0bfb2784ce3b5f4773326d5008b03624437
rickycodes <ricky.miller@gmail.com>
2018-04-16 17:19:56 -0400

    Add test

--- test.js
+++ test.js
@@ -5,3 +5,13 @@ test('should be promise', t => {
   t.plan(1);
   t.equal(typeof wait().then, 'function');
 });
+
+test('should wait', t => {
+  t.plan(1);
+  const track = [];
+  wait(10).then(_ => {
+    track.push('b');
+    t.deepEqual(['a', 'b'], track);
+  })
+  track.push('a');
+});

4ec8c274ac1af69918df56ea1e10c0f1c9b7a771
tylergreulich <tgreul6@gmail.com>
2018-10-31 08:50:03 -0700

    Added typescript declaration file

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * @param amount The amount of time to wait in milliseconds.
+ * @return A promise that gets resolved after a given amount.
+ */
+export function wait(amount: number): Promise<void>;
